8a5d2f7a54408af2f75c668586ded42f7121fae5
Joe Lencioni <jlencion@gustavus.edu>
2012-02-08 14:40:16 -0600

    Added magic TestObject class

---
+++ TestObject.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package Test
+ */
+
+namespace Gustavus\Test;
+
+require_once 'Gustavus/Test/TestLib.php';
+
+/**
+ * @package Test
+ */
+class TestObject
+{
+  /**
+   * @var object
+   */
+  private $object;
+
+  /**
+   * @param object $object
+   */
+  public function __construct($object)
+  {
+    $this->object = $object;
+  }
+
+  /**
+   * @param string $method
+   * @param array $arguments
+   * @return mixed
+   */
+  public function __call($method, array $arguments)
+  {
+    if (is_callable(array($this->object, $method))) {
+      return call_user_func_array(array($this->object, $method), $arguments);
+    } else {
+      return TestLib::call($this->object, $method, $arguments);
+    }
+  }
+
+  /**
+   * @param string $property
+   * @param mixed $value
+   * @return mixed
+   */
+  public function __set($property, $value)
+  {
+    if (is_callable(array($this->object, '__set'))) {
+      return $this->object->__set($property, $value);
+    } else {
+      return TestLib::set($this->object, $property, $value);
+    }
+  }
+
+  /**
+   * @param string $property
+   * @return mixed
+   */
+  public function __get($property)
+  {
+    if (is_callable(array($this->object, '__get'))) {
+      return $this->object->__get($property);
+    } else {
+      return TestLib::get($this->object, $property);
+    }
+  }
+}

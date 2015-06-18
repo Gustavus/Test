dbb5edc67536046b8104ded4ed4c679bc20cd263
Justin Holcomb <jholcom2@gustavus.edu>
2015-06-18 14:02:58 -0500

    Added static getDBH

--- TestDBPDO.php
+++ TestDBPDO.php
@@ -15,6 +15,14 @@ abstract class TestDBPDO extends TestDB
    */
   final protected function getDBH()
   {
+    return self::getDBHStatic();
+  }
+
+  /**
+   * Static implementation of getDBH()
+   */
+  final static protected function getDBHStatic()
+  {
     if (self::$dbh === null) {
       self::$dbh = new \PDO('sqlite::memory:');
     }
